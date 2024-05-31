104bfd755103866836af27efa18a656b746892b6
namacosansan <namacosansan@gmail.com>
2024-05-31 13:42:46 +0900

    Create a calendar

---
+++ 02.calendar/calendar.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'date'
+
+def calendar(year, month)
+  first_day = Date.new(year, month, 1)
+  last_day = Date.new(year, month, -1)
+  today = Date.today
+
+  puts  "     #{month}月 #{year}"
+  puts "日 月 火 水 木 金 土"
+
+  (first_day..last_day).each do |date|
+    print "   " * date.wday if date == first_day
+
+    if date.saturday? && date == today
+      print "{\e[7m#{date.day.to_s.rjust(2)}\e[0m }\n"
+    elsif  date.saturday? 
+      print "#{date.day.to_s.rjust(2)}\n"
+    elsif  date == today
+      print "\e[7m#{date.day.to_s.rjust(2)}\e[0m "
+    else
+      print "#{date.day.to_s.rjust(2)} "
+    end
+  end
+  print "\n" unless last_day.saturday? 
+end
+
+command = ARGV.getopts("y:m:")
+year = command["y"] ? command["y"].to_i : Date.today.year
+month = command["m"] ? command["m"].to_i : Date.today.month
+
+calendar(year, month)
+
+
