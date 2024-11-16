4e08db870982e5855ebd8584ff151e500e138e5a
namacosansan <namacosansan@gmail.com>
2024-11-16 15:35:34 +0900

    Add COL_SIZE constant for configuring number of rows

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,18 +1,24 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+COL_SIZE = 3
+
 def fetch_files
   Dir.glob('*')
 end
 
 def calculate_slice_size(files)
-  (files.size / 3.0).ceil
+  (files.size / COL_SIZE.to_f).ceil
 end
 
 def create_slices(files, slice_size)
   slices = files.each_slice(slice_size).to_a
   max_length = slices.map(&:length).max
   slices.each { |slice| slice.fill(nil, slice.length...max_length) }
+end
+
+def calculate_slices_size(slices)
+  slices.size
 end
 
 def transpose_slices(slices)
@@ -33,8 +39,8 @@ def display_length(str)
   str.chars.sum { |char| char.bytesize > 1 ? 2 : 1 }
 end
 
-def format_and_print_files(files, max_length)
-  files.each_slice(3) do |group|
+def format_and_print_files(files, max_length, slices_size)
+  files.each_slice(slices_size) do |group|
     puts group.map { |file|
       next '' if file.nil?
 
@@ -47,7 +53,8 @@ end
 files = fetch_files
 slice_size = calculate_slice_size(files)
 slices = create_slices(files, slice_size)
+slices_size = calculate_slices_size(slices)
 transposed = transpose_slices(slices)
 flattened = flatten_and_clean(transposed)
 max_length = max_file_length(flattened)
-format_and_print_files(flattened, max_length).compact
+format_and_print_files(flattened, max_length, slices_size).compact
